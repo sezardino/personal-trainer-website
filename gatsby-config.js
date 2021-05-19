2981504c00e644c88005bad18ba2b2a3deac7f65
Edward <e.arechwa@gmail.com>
2021-05-19 21:54:08 +0200

    fix node files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,84 @@
 module.exports = {
-    siteMetadata: {
-        title: "Robert Arechwa personal trainer",
-        description: "this site wac created from gatsby starter",
+  siteMetadata: {
+    title: "Robert Arechwa - Trener Personalny",
+    description: `Hej, mam na imie Robert Arechwa, jestem trenerem personalnym, pomoge ci zadbać o swoją sylwetke.
+        Prowadze treningi personalne, oraz mogę rospisać dla ciebię diete`,
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "uploads",
+      },
     },
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sass",
-        {
-            // keep as first gatsby-source-filesystem plugin for gatsby image support
-            resolve: "gatsby-source-filesystem",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/site`,
+        name: "site",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/img`,
+        name: "images",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
             options: {
-                path: `${__dirname}/static/img`,
-                name: "uploads",
+              name: "uploads",
             },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
+          },
+          {
+            resolve: "gatsby-remark-images",
             options: {
-                path: `${__dirname}/src/pages`,
-                name: "pages",
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
             },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
+          },
+          {
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-                path: `${__dirname}/src/img`,
-                name: "images",
+              destinationDir: "static",
             },
-        },
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-        {
-            resolve: "gatsby-transformer-remark",
-            options: {
-                plugins: [
-                    {
-                        resolve: "gatsby-remark-relative-images",
-                        options: {
-                            name: "uploads",
-                        },
-                    },
-                    {
-                        resolve: "gatsby-remark-images",
-                        options: {
-                            // It's important to specify the maxWidth (in pixels) of
-                            // the content container as this plugin uses this as the
-                            // base for generating different widths of each image.
-                            maxWidth: 2048,
-                        },
-                    },
-                    {
-                        resolve: "gatsby-remark-copy-linked-files",
-                        options: {
-                            destinationDir: "static",
-                        },
-                    },
-                ],
-            },
-        },
-        {
-            resolve: "gatsby-plugin-netlify-cms",
-            options: {
-                modulePath: `${__dirname}/src/cms/cms.js`,
-            },
-        },
-        {
-            resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
-            options: {
-                develop: true, // Activates purging in npm run develop
-                purgeOnly: ["/style.scss"], // applies purging only on the bulma css file
-            },
-        }, // must be after other CSS plugins
-        "gatsby-plugin-netlify", // make sure to keep it last in the array
-    ],
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
+      options: {
+        develop: true, // Activates purging in npm run develop
+        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+      },
+    }, // must be after other CSS plugins
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
+  ],
 };
