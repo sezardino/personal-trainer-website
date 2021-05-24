fcd99ce0fd56e33370aa8b32aefae80e8bf26837
Edward <edward.arechwa@gogomedia.pl>
2021-05-24 13:09:55 +0200

    bind hero section with cms

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,30 +1,46 @@
 import React from "react";
+import { stringFormatting } from "../utils";
 import Social from "./social";
 
-const Hero = ({ image }) => {
-  return (
-    <section
-      className="hero"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-      }}
-    >
-      <div className="container hero__container">
-        <h1 className="hero__title">
-          <span className="hero__title-line"><span className="hero__title-frst">R</span>obert</span>
-          <span className="hero__title-line"><span className="hero__title-frst">A</span>rechwa</span>
-          <span className="hero__title-line hero__title-line--last">
-            trener personalny
-          </span>
-        </h1>
-        <div className="hero__social">
-          <Social extraClass="hero"/>
-        </div>
-      </div>
-    </section>
-  );
+const getTitleTemplate = (title) =>
+    stringFormatting.split(title).map((word) => {
+        return (
+            <span className="hero__title-line" key={word}>
+                <span className="hero__title-frst">
+                    {stringFormatting.getFirstLetters(word)}
+                </span>
+                {stringFormatting.deleteFirstLetters(word)}
+            </span>
+        );
+    });
+
+const Hero = ({ data }) => {
+    const { description, image, title } = data;
+
+    return (
+        <section
+            className="hero"
+            style={{
+                backgroundImage: `url(${
+                    !!image.childImageSharp
+                        ? image.childImageSharp.fluid.src
+                        : image.publicURL
+                })`,
+            }}
+        >
+            <div className="container hero__container">
+                <h1 className="hero__title">
+                    {getTitleTemplate(title)}
+                    <span className="hero__title-line hero__title-line--last">
+                        {description}
+                    </span>
+                </h1>
+                <div className="hero__social">
+                    <Social extraClass="hero" />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
