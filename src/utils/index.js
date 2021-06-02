6941e136b746884f1741f3722eba55b3f534e7c6
Edward <edward.arechwa@gogomedia.pl>
2021-06-02 10:23:06 +0200

    update utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,15 @@
 const stringFormatting = {
     split(string) {
-        return string.split(" ");
+        const stringArr = string.split(" ");
+        const oneLetter = stringArr.find((item) => item.length === 1);
+        if (oneLetter) {
+            const index = stringArr.findIndex((item) => item === oneLetter);
+            stringArr[index + 1] = `${stringArr[index]} ${
+                stringArr[index + 1]
+            }`;
+            return stringArr.filter((item) => item !== oneLetter);
+        }
+        return stringArr;
     },
     getFirstLetters(string) {
         return string.slice(0, 1);
