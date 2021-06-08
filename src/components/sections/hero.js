6291dbfa216b49685b02a9e54b923eb96785d457
Edward <edward.arechwa@gogomedia.pl>
2021-06-08 08:54:27 +0200

    refactor hero section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -2,33 +2,52 @@ import React from "react";
 import { stringFormatting } from "../../utils";
 import Social from "../parts/social";
 
-const getTitleTemplate = (title) =>{
-const wordsArr  = stringFormatting.split(title)
+const getTitleTemplate = (title) => {
+    const wordsArr = stringFormatting.split(title);
     return wordsArr.map((word) => {
         return (
-            <span className={`hero__title-line ${wordsArr.length === 1 ? 'hero__title-line--one': ''}`} key={word}>
+            <span
+                className={`hero__title-line ${
+                    wordsArr.length === 1 ? "hero__title-line--one" : ""
+                }`}
+                key={word}
+            >
                 <span className="hero__title-frst">
                     {stringFormatting.getFirstLetters(word)}
                 </span>
                 {stringFormatting.deleteFirstLetters(word)}
             </span>
         );
-    })};
+    });
+};
 
-const Hero = ({ data, isHome }) => {
+const Hero = ({ data, extraClass }) => {
     const { description, image, title } = data;
     let titleTemplate;
 
-    if (data.titleFormat) {
-        titleTemplate =
-            data.titleFormat === "breaking" ? getTitleTemplate(title) : title;
+    if (extraClass === "home") {
+        titleTemplate = (
+            <h1 className="hero__title">
+                {getTitleTemplate(title)}
+                <span className="hero__title-line hero__title-line--last">
+                    {description}
+                </span>
+            </h1>
+        );
     } else {
-        titleTemplate = getTitleTemplate(title);
+        titleTemplate = (
+            <>
+                <h1 className="hero__title">{getTitleTemplate(title)}</h1>
+                <p className="hero__title-line hero__title-line--last">
+                    {description}
+                </p>
+            </>
+        );
     }
 
     return (
         <section
-            className={`hero${isHome ? ' hero--home' : ''}`}
+            className={`hero${extraClass ? " hero--" + extraClass : ""}`}
             style={{
                 backgroundImage: `url(${
                     !!image.childImageSharp
@@ -38,12 +57,7 @@ const Hero = ({ data, isHome }) => {
             }}
         >
             <div className="container hero__container">
-                <h1 className="hero__title">
-                    {titleTemplate}
-                    <span className="hero__title-line hero__title-line--last">
-                        {description}
-                    </span>
-                </h1>
+                {titleTemplate}
                 <div className="hero__social">
                     <Social extraClass="hero" />
                 </div>
