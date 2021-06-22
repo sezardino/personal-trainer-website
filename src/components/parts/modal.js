936632765bddc225a1b12ed09e9148071af73911
Edward <edward.arechwa@gogomedia.pl>
2021-06-22 08:45:22 +0200

    gallery

--- src/components/parts/modal.js
+++ src/components/parts/modal.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Loader from "./loader";
 import Portal from "./portal";
 
 const Modal = ({ data, show, closeHandler }) => {
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
+        setLoading(true);
         document.body.style.overflow = show ? "hidden" : "initial";
     }, [show]);
 
@@ -11,12 +14,15 @@ const Modal = ({ data, show, closeHandler }) => {
             <div className={`modal ${show ? "modal--show" : ""}`}>
                 <div className="modal__overflow" onClick={closeHandler}></div>
                 <div className="modal__inner">
-                    <h3 className="modal__title">{data?.name}</h3>
+                    {loading && <Loader />}
                     <img
                         src={data?.image.childImageSharp.fluid.src}
                         alt={data?.name}
                         className="modal__img"
+                        title={data?.name}
+                        onLoad={() => setLoading(false)}
                     />
+                    <h3 className="modal__title">{data?.name}</h3>
                 </div>
             </div>
         </Portal>
